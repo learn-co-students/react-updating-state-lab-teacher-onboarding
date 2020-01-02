a11848868090a192e2241c9de7123467d7ca1541
Marc Majcher <majcher@gmail.com>
2020-01-02 11:45:05 -0600

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,18 @@
-// Code DigitalClicker Component Here
+import React, { Component } from "react";
+
+export default class DigitalClicker extends Component {
+  constructor() {
+    super();
+    this.state = {
+      timesClicked: 0
+    };
+  }
+
+  render() {
+    const updateCount = () => {
+      this.setState({ timesClicked: this.state.timesClicked + 1 });
+    };
+
+    return <button onClick={updateCount}>{this.state.timesClicked}</button>;
+  }
+}
